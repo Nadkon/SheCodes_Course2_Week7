0d1039c1dbbd967eaa39251d1be39f564379be77
“Nadiia <nadkon@gmail.com>
2022-12-07 21:29:21 +0200

    update: form search

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Sohw current date and time (version 2)
+// Sohw current date and time 
 function FormatDate(timestamp){
     let date = new Date (timestamp);
     let hours = date.getHours();
@@ -33,37 +33,17 @@ function displayTemperature(response){
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function search (city){
+function search(city){
     let apiKey = "3324937c27278e13f542f63f7e3df9b5";
     let unit = "metric";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
     axios.get(apiUrl).then(displayTemperature);
 }
 
-// Show current date and time (version 1)
-// let now = new Date();
-// let days = ["Sunday", "Monday", "Tuesdya", "Wednesday", "Thursday", "Friday", "Saturday"];
-// let weekDay = document.querySelector("#week-day");
-// weekDay.innerHTML = days[now.getDay()];
-
-// let hourElement = document.querySelector("#hour");
-// let hour =  now.getHours();
-//     if (hour < 10){
-//         hour = `0${hour}`
-//     };
-// hourElement.innerHTML = hour;
-// let minuteElement = document.querySelector("#minutes");
-// let minute = now.getMinutes();
-//     if (minute < 10){
-//         minute = `0${minute}`
-//     };
-// minuteElement.innerHTML = minute;
-
-// Form search
 function searchCity(event){
     event.preventDefault();
     let cityInputElement = document.querySelector("#city-input");
-    console.log(cityInputElement.value);
+    search(cityInputElement.value);
 }
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchCity);
