b8373e9198755e2a0dc297b94c2e01bd48a6efdb
“Nadiia <nadkon@gmail.com>
2022-12-07 19:51:54 +0200

    update: added city variable in js

--- src/index.js
+++ src/index.js
@@ -17,13 +17,13 @@ function FormatDate(timestamp){
 // Show current temperature
 function displayTemperature(response){
     let currentTemperature = document.querySelector("#current-temp");
-    let city = document.querySelector("#city");
+    let cityElement = document.querySelector("#city");
     let description = document.querySelector("#description");
     let humidity = document.querySelector("#humidity");
     let wind = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
     currentTemperature.innerHTML = Math.round(response.data.main.temp);
-    city.innerHTML = response.data.name;
+    cityElement.innerHTML = response.data.name;
     description.innerHTML = response.data.weather[0].description;
     humidity.innerHTML = response.data.main.humidity;
     wind.innerHTML = Math.round(response.data.wind.speed);
@@ -31,7 +31,8 @@ function displayTemperature(response){
 }
 let apiKey = "3324937c27278e13f542f63f7e3df9b5";
 let unit = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=${unit}`;
+let city = "Kyiv";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
 axios.get(apiUrl).then(displayTemperature);
 
 // Show current date and time (version 1)
